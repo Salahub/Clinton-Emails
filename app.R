a8d7389efed80aadb0b2ca318d62c01204c136e6
Salahub <cdsalahub@gmail.com>
2017-04-02 13:44:24 -0400

    Formatting and write up updates

--- app.R
+++ app.R
@@ -178,7 +178,8 @@ ui <- fluidPage(
    tags$head(tags$script(jscode)),
 
    # Application title
-   titlePanel("Secretary Clinton's Email (Source: https://wikileaks.org/clinton-emails/)"),
+   titlePanel(HTML("Secretary Clinton's Email (Source: <a href = 'https://wikileaks.org/clinton-emails/'>Wikileaks</a>)")),
+   fluidRow(column(h4("Christopher D. Salahub and R. Wayne Oldford"), offset = 0.2, width = 12)),
 
    # the slider right below the title to make it as long as possible
    fluidRow(column(width = 2, offset = 0.5, h4("Date Range:"))),
@@ -194,107 +195,62 @@ ui <- fluidPage(
      # well panel with write up
      column(width = 4,
             wellPanel(id = "WriteUp", 
-                      style = "overflow-y:scroll; max-height: 800px", 
-                      h3("A Brief Overview"),
-                      h5("Christopher D. Salahub and R. Wayne Oldford"),
-                      h3("The Email Timeline"),
-                      p("While the following timeline is somewhat abbreviated, it should serve
-                        to raise some of the major issues and concerns related to Clinton’s
-                        private email server and its contents. It also introduces some of the
-                        principal characters involved. More complete and in depth timelines
-                        are readily available elsewhere (e.g. ", 
-                        a("Sharyl Attkisson",  
-                          href = "https://sharylattkisson.com/hillary-clintons-email-the-definitive-timeline/"),
-                        " or ", 
-                        a("The Washington Post",
-                          href = "https://www.washingtonpost.com/news/fact-checker/wp/2015/03/10/hillary-clintons-emails-a-timeline-of-actions-and-regulations/?utm_term=.dec3139a0542"),
-                        ") On November 21, 2008, the New York Times reported that Hillary
-                        Clinton had decided to accept the position of U.S. Secretary of State.
-                        On January 13, 2009 the internet domain name clintonemail.com
-                        was ",
-                        a("registered", href = "https://www.whois.net/"), 
-                        " and eight days later Senator Clinton was confirmed as
-                        Secretary of State."),
-                      p("Public knowledge that a private email server was being used by
-                        Secretary Clinton and others for State Department and personal communications
-                        did not surface until ",
-                        a("March 2015", href = "https://www.nytimes.com/2015/03/03/us/politics/hillary-clintons-use-of-private-email-at-state-department-raises-flags.html"),
-                        " during the course of a ", 
-                        a("U.S. Congressional investigation", href = "http://benghazi.house.gov/"), 
-                        " of the September 11, 2012 attack by militants on U.S. compounds in 
-                        Benghazi Libya. The State Department had ",
-                        a("difficulty fulfilling", href = "https://www.washingtonpost.com/investigations/how-clintons-email-scandal-took-root/2016/03/27/ee301168-e162-11e5-846c-10191d1fc4ec_story.html?utm_term=.95919beefdc0"),
-                        " public FOIA and House Benghazi Committee requests for Secretary Clinton’s
-                        government emails because she had exclusively used the private server
-                        for all her email. On March 10, 2015, Clinton told reporters that she
-                        turned over 30,490 emails to the State Department and deleted 31,830
-                        emails deemed to be personal. Clinton had tasked ",
-                        a("three lawyers", href = "http://www.dailymail.co.uk/news/article-3773393/Clinton-s-former-Chief-Staff-served-attorney-private-server-investigation.html"),
-                        " Cheryl Mills (Clinton’s former chief of staff), David Kendall (Clinton’s
-                        personal lawyer), and Heather Samuelson (a State Department staffer
-                        during Clinton’s tenure) to make the determinations as to which emails
-                        were work related and which were not."),
-                      p("On March 10, 2015 the House Benghazi Committee ",
-                        a("requested", href = "https://www.nytimes.com/politics/first-draft/2015/03/20/house-benghazi-committee-requests-hillary-clinton-email-server/"),
-                        " that the private email server be turned over to a neutral third party
-                        to determine which emails are personal and which are government
-                        records, but was informed March 27 by David Kendall that ",
-                        a("no emails remained", href = "https://www.nytimes.com/2015/03/28/us/politics/no-copies-of-hillary-clinton-emails-on-server-lawyer-says.html"),
-                        " on the private server for any kind of review.
-                        Between March 25 and 31, 2015, Paul Combetta (then the server’s
-                        system administrator), erased all backup copies using BleachBit (see
-                        www.bleachbit.org)."),
-                      p(a("Combetta,", href = "https://www.nytimes.com/2016/09/09/us/politics/hillary-clinton-emails-investigation.html"),
-                        a("Mills,", href = "http://www.politico.com/story/2016/09/mills-immunity-228580"),
-                        " and ",
-                        a("Samuelson", href = "http://dailycaller.com/2016/09/23/the-immunized-five-meet-the-people-covering-for-hillary/"),
-                        " were later granted partial immunity by the Justice Department during
-                        the FBI investigations into the private email server, as were two others: ",
-                        a("Bryan Pagliano", href = "http://dailycaller.com/2016/09/23/the-immunized-five-meet-the-people-covering-for-hillary/"),
-                        " (original server manager) and ",
-                        a("John Bentel", href = "http://dailycaller.com/2016/09/23/the-immunized-five-meet-the-people-covering-for-hillary/"),
-                        " (former director of Information Resources
-                        Management for the State Department’s Executive Secretariat)"),
-                      p("On April 12, 2015 Clinton announces that she is running for the
-                        U.S. Presidency. On July 24, 2015, inspectors general for the State
-                        Department and the national intelligence agencies announce that they ",
-                        a("found classified information", href = "https://www.nytimes.com/2015/07/25/us/politics/hillary-clinton-email-classified-information-inspector-general-intelligence-community.html"),
-                        "in the emails and that the information was classified at the time sent,
-                       though her campaign declared that they must have been classified after
-                       the fact. On August 19, 2015, Clinton calls the allegation of mishandling
-                       classified information a ",
-                        a("'disagreement between agencies'", href = "https://www.theguardian.com/us-news/2015/aug/19/hillary-clinton-dismisses-misconduct-private-email")),
-                      p("Nearly one year later, July 5, 2016, FBI Director James Comey
-                       recommended that ",
-                        a("no charges be laid", href = "https://www.nytimes.com/2016/07/06/us/politics/hillary-clinton-fbi-email-comey.html"),
-                        "against Clinton for her use of a private email server. On 
-                       October 28, 2016, Comey revealed that
-                       in a separate investigation into former Congressman Anthony Weiner,
-                       that emails belonging to his wife Huma Abedin had been found on
-                       his laptop. Since Abedin was a close aid to former Secretary Clinton,
-                       FBI investigations were reopened into the private server usage but
-                       closed again by Comey on November 6, 2016 ",
-                        a("without charges being laid.", href = "https://www.theguardian.com/us-news/2016/nov/06/fbi-director-hillary-clinton-email-investigation-criminal-james-comey"),
-                        "In both cases, Comey and the FBI are criticized by pundits
-                       from different political parties."),
-                      h3("Wikipedia Articles of Key Players"),
-                      dataTableOutput("links"),
-                      h3("The Service"),
-                      p("This application provides the ability to interactively filter emails sent
-                       during Hillary Clinton's tenure as the United States Secretary of State and
-                       display features of the selected subset.The data is extracted from
-                       HTML representations of the ",
-                        a("official State Department release", href = "https://foia.state.gov/Search/Results.aspx?collection=Clinton_Email"),
-                        "provided in a ",
-                        a("Wikileaks data base.", href = "https://wikileaks.org/clinton-emails/")),
-                      p("This service is not meant to provide stand-alone means of analyzing this
-                       controversial data set. It is most powerful when used simultaneously with
-                       both internet searches and the Wikileaks data base or official State 
-                       Department site. The latter two services provide indispensable context
-                       and precision respectively; two services which the primarily 
-                       metadata-driven displays cannot provide. Rather, the intended use of this
-                       application is the exploration of patterns present in the data to
-                       generate and explore different hypotheses."))),
+                      style = "overflow-y:scroll; max-height: 800px",
+                      fluidRow(id = "Contents", h3("Table of Contents"),
+                               p(HTML("<a href='#KeyPlayers'>Wikipedia Articles of Key Players</a>")),
+                               p(HTML("<a href='#Timeline'>Timeline Links</a>")),
+                               p(HTML("<a href='#Context'>Context Links</a>")),
+                               p(HTML("<a href='#App'>Service Description</a>")),
+                               p(HTML("<a href='#Data'>Data</a>")),
+                               p(HTML("<a href='#Analysis'>Some Interesting Analyses</a>"))),
+                      fluidRow(id = "KeyPlayers", 
+                               h3("Wikipedia Articles of Key Players"),
+                               p(HTML("<a href='#Contents'>Back to table of contents</a>")),
+                               p(HTML(paste(links, collapse = " | ")))),
+                      fluidRow(id = "Timeline", h3("The Email Timeline"),
+                               p(HTML("<a href='#Contents'>Back to table of contents</a>")),
+                               a("Sharyl Attkisson Timeline",  
+                                 href = "https://sharylattkisson.com/hillary-clintons-email-the-definitive-timeline/"),
+                               " | ",
+                               a("The Washington Post", href = "https://www.washingtonpost.com/news/fact-checker/wp/2015/03/10/hillary-clintons-emails-a-timeline-of-actions-and-regulations/?utm_term=.dec3139a0542"),
+                               " | ",
+                               a("Wikipedia", href = "https://en.wikipedia.org/wiki/Hillary_Clinton_email_controversy")),
+                      fluidRow(id = "Context", h3("Useful Context"),
+                               p(HTML("<a href='#Contents'>Back to table of contents</a>")),
+                               a("Clinton's tenure as Secretary of State",
+                                 href = "https://en.wikipedia.org/wiki/Hillary_Clinton%27s_tenure_as_Secretary_of_State"),
+                               " | ",
+                               a("2011 Benghazi attack",
+                                 href = "https://en.wikipedia.org/wiki/Timeline_of_the_investigation_into_the_2012_Benghazi_attack#October_2012")),
+                      fluidRow(id = "App", h3("The Service"),
+                               p(HTML("<a href='#Contents'>Back to table of contents</a>")),
+                               p("This application provides the ability to interactively filter 32,795 emails
+                                 sent during Hillary Clinton's tenure as the United States Secretary of 
+                                 State and display features of the selected subset. The data is extracted
+                                 from HTML representations of the ",
+                                 a("official State Department release", href = "https://foia.state.gov/Search/Results.aspx?collection=Clinton_Email"),
+                                 "provided in a ",
+                                 a("Wikileaks data base.", href = "https://wikileaks.org/clinton-emails/")),
+                               p("This service is not meant to provide stand-alone means of analyzing this
+                                 controversial data set. It is most powerful when used simultaneously with
+                                 both internet searches and the Wikileaks data base or official State 
+                                 Department site. The latter two services provide indispensable context
+                                 and precision; two services which the primarily 
+                                 metadata-driven displays cannot provide. Rather, the intended use of this
+                                 application is the exploration of patterns present in the data to
+                                 generate and explore different hypotheses.")),
+                      fluidRow(id = "Data", h3("The Data"),
+                               p(HTML("<a href='#Contents'>Back to table of contents</a>")),
+                               p("This application allows the user to view the senders, recipients, times, ",
+                                 a("Freedom of Information Act exemption codes",
+                                   href = "https://vault.fbi.gov/explanation-of-exemptions"),
+                                 "(applied by the State Department censors in the official release), the
+                                 most frequent terms, and the most important terms as measured by the ", 
+                                 a("tf-idf", href = "https://en.wikipedia.org/wiki/Tf%E2%80%93idf"),
+                                 "over the filtered region selected.")),
+                      fluidRow(id = "Analysis", 
+                               h3("Some Interesting Analyses"),
+                               p(HTML("<a href='#Contents'>Back to table of contents</a>"))))),
 
      # central interaction panel with a slider input for number of bins
      column(width = 2,
@@ -304,7 +260,8 @@ ui <- fluidPage(
             fluidRow(selectInput("ToFromFilter", "Show Emails: ", 
                                  c("From Clinton", "To Clinton", "All Emails"),
                                  selected = "All Emails", multiple = FALSE)),
-            fluidRow(selectInput("ClassFilter", "Emails Including Any of the FOIA Codes: ",
+            fluidRow(selectInput("ClassFilter", 
+                                 HTML("Contains any of the <a href='https://vault.fbi.gov/explanation-of-exemptions'>FOIA codes</a>:"),
                                  c("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "None"),
                                  selected = c("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", 
                                               "B9", "None"),
@@ -368,28 +325,28 @@ server <- function(input, output) {
      # first determine whether adjusted or unadjusted times are to be used
      if (inter()$adjVar) {
        timevalues <- AsSec$Hour6pm[AsSec$ID %in% inter()$selIDs]*60 + 
-         AsSec$Minutes[AsSec$ID %in% inter()$selIDs,]
+         AsSec$Minutes[AsSec$ID %in% inter()$selIDs]
        xlab <- "Adjusted Date (dd/mm/yy)"
        main <- paste("Adjusted Email", inter()$toFromLab, "Times")
-       labelset <- c('22:00', '02:00', '06:00', '10:00', '14:00')
+       labelset <- c('18:00', '24:00', '06:00', '12:00', '18:00')
      } else {
        timevalues <- AsSec$Hour[AsSec$ID %in% inter()$selIDs]*60 + 
          AsSec$Minutes[AsSec$ID %in% inter()$selIDs]
        xlab <- "Date (dd/mm/yy)"
        main <- paste("Email", inter()$toFromLab, "Times")
-       labelset <- c("04:00", "08:00", "12:00", "16:00", "20:00")
+       labelset <- c("00:00", "06:00", "12:00", "18:00", "24:00")
      }
      # plot dates based on the dates provided from the slider
      plot(x = as.chron(floor(AsSec$Date[AsSec$ID %in% inter()$selIDs])),
           y = timevalues, xlab = xlab,
           ylab = paste("Time", inter()$toFromLab), yaxt = 'n', xaxt = 'n', pch = 19,
-          main = paste(main, " (", inter()$DateRange, ")", sep = ""),
+          main = main, sub = inter()$DateRange,
           col = adjustcolor(pal[as.numeric(AsSec$Redacted[AsSec$ID %in% inter()$selIDs])+1], 
                             alpha.f = 0.5),
           cex = 0.25, ylim = c(1440,0) + c(0.05,-0.05)*1440, 
           xlim = c(input$range[1], input$range[2]))
-     axis(side = 2, at = c(240, 480, 720, 960, 1200),
-          labels = labelset)
+     axis(side = 2, at = c(0, 360, 720, 1080, 1440),
+          labels = labelset, las = 1)
      axis.Date(side = 1, as.chron(c(input$range[1], input$range[2])), format = "%d/%m/%y")
      legend("topright", legend = c("Redacted", "Unedited"), pch = c(19,19),
            col = c("firebrick", "steelblue"), horiz = TRUE, cex = 0.8, inset = c(0,-0.05),
@@ -399,7 +356,7 @@ server <- function(input, output) {
    output$DaySum <- renderPlot({
      plot(x = inter()$selDays, y = inter()$selCounts, type = 'l', xlab = 'Date (dd/mm/yy)',
           xaxt = "n", ylab = 'Number of Emails', pch = 19,
-          main = paste("Number of Emails by Date (", inter()$DateRange, ")", sep = ""),
+          main = "Number of Emails by Date", sub = inter()$DateRange,
           ylim = extendrange(AScounts), col = adjustcolor("black", alpha.f = 0.6))
      # add the schedule if it has been selected
      if (inter()$dispSched) {
@@ -445,8 +402,8 @@ server <- function(input, output) {
    # finally a barplot of classification codes used in the selection
    output$Class <- renderPlot(
      barplot(table(unlist(str_split(AsSec$Classification[AsSec$ID %in% inter()$selIDs], "-"))),
-                          main = paste("FOIA Exemption Codes Used for Redactions (",
-                                       inter()$DateRange, ")", sep = ""))
+                          main = "FOIA Redaction Codes Appearing in Selected Emails",
+                          sub = inter()$DateRange)
    )
    # record the user window size (still cannot use in ui)
    # output$winHeight <- reactive(input$height)
